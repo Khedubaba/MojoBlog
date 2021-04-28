3d67e7a4c4771d72146a706f55b001c24b2a7faa
khedubaba <adityakhedekar49@gmail.com>
2021-04-28 09:13:18 +0530

    useState Hook

--- mojo-blog/src/Home.js
+++ mojo-blog/src/Home.js
@@ -1,18 +1,28 @@
+import { useState } from 'react';
+
 const Home = () => {
 
-    const handleClick = (e) => {
-        console.log("Hello Bloggers.", e);
-    }
+    // let name = 'luiji';
+    const [name, setName] = useState('mario');
+    const [age, setAge] = useState(24);
 
-    const handleClickAgain = (name, e) => {
-        console.log("hello", name, e.target);
-    }
+        const handleClick = (e) => {
+            setName('luiji');
+            setAge(30);
+        }
+
+    // const handleClickAgain = (name, e) => {
+    //     console.log("hello", name, e.target);
+    // }
+
+
 
     return (
         <div className="home">
             <h2>Homepage</h2>
+            <p>{name} is {age} years old.</p>
             <button onClick = {handleClick}>Click Me</button>
-            <button onClick = {(e) => handleClickAgain('mario', e)}>Click Me Again</button>
+            {/* <button onClick = {(e) => handleClickAgain('mario', e)}>Click Me Again</button> */}
         </div>
     );
 }
