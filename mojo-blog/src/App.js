e902cab84fd7d51a4861aff9ee1a585f54ef2214
khedubaba <adityakhedekar49@gmail.com>
2021-04-29 10:06:16 +0530

    Routing and resuing useFetch hook

--- mojo-blog/src/App.js
+++ mojo-blog/src/App.js
@@ -1,18 +1,32 @@
 import Navbar from './Navbar';
 import Home from './Home';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Create from './Create';
+import BlogDetails from './BlogDetails';
 
 function App() {
   // const title = "Welcome to the new blog";
   // const person = { name: 'yoshi', age: 30};
 
   return (
-    <div className="App">
-    <Navbar />
-      <div className="content">
-        {/* <h1>{ title }</h1> */}
-        <Home />
+    <Router>
+      <div className="App">
+      <Navbar />
+        <div className="content">
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/create">
+              <Create />
+            </Route>
+            <Route path="/blogs/:id">
+              <BlogDetails />
+            </Route>
+          </Switch>
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
--- mojo-blog/src/Home.js
+++ mojo-blog/src/Home.js
@@ -1,31 +1,8 @@
-import { useState, useEffect} from 'react';
 import BlogList from './BlogList';
+import useFetch from './useFetch';
 
 const Home = () => {
-    const [blogs, setBlogs] = useState(null);
-    const [isPending, setIsPending] = useState(true);
-    const [error, setError] = useState(null);
-
-    useEffect(() => {
-        setTimeout(() => {
-            fetch('http://localhost:8000/blogs')
-                .then(res => {
-                    if (!res.ok) {
-                        throw Error('Could not fecth data for the resource');
-                    }
-                    return res.json();
-                })
-                .then(data => {
-                    setBlogs(data);
-                    setIsPending(false);
-                    setError(null);
-                })
-                .catch(err => {
-                    setIsPending(false);
-                    setError(err.message);
-                });
-        }, 1000);
-    }, []);
+    const {data:blogs, isPending, error} = useFetch('http://localhost:8000/blogs');
 
     return (
         <div className="home">
@@ -35,5 +12,5 @@ const Home = () => {
         </div>
     );
 }
- 
+
 export default Home;
